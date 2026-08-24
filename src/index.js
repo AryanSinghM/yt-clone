72e890d7bc65897182d4bed2fbd6483407c59b1b
AryanSinghM <aryansingh8545937396@gmail.com>
2026-08-25 00:00:43 +0530

    utils discussion

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+dotenv.config()
+import connectDB from "./db/index.js"
+import app from "./app.js"
+import dns from "dns"
+
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("mongodb connection failed !!!")
+})
+
+
