9e50e1a244248b279aec9d5c457b65f3cb917ef6
AryanSinghM <aryansingh8545937396@gmail.com>
2026-08-26 22:54:57 +0530

    cloudinary, multer, registeruser and routes

--- src/app.js
+++ src/app.js
@@ -18,4 +18,19 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import userRouter from "./routes/user.route.js";
+
+
+app.use("/api/v1/users", userRouter)
+
+
+
+
+
+
+
+
+
+
 export default app
